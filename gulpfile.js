64d924e061c536698eb0d2ece43a3800deb88394
sergei <kvakazuabr@yandex.ru>
2018-11-01 23:34:16 +0300

    yflt

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,118 @@
+// "use strict";
+
+// var gulp = require("gulp");
+// var sass = require("gulp-sass");
+// var plumber = require("gulp-plumber");
+// var postcss = require("gulp-postcss");
+// var autoprefixer = require("autoprefixer");
+// var server = require("browser-sync").create();
+// var minify = require("gulp-csso");
+// var rename = require("gulp-rename");
+// var imagemin = require("gulp-imagemin");
+// var posthtml = require("gulp-posthtml");
+// var include = require("posthtml-include");
+// var del = require("del");
+// var run = require("run-sequence");
+// var svgstore = require("gulp-svgstore");
+// var webp = require("gulp-webp");
+
+// gulp.task("css", function () {
+//   return gulp.src("source/sass/style.scss")
+//     .pipe(plumber())
+//     .pipe(sass())
+//     .pipe(postcss([
+//       autoprefixer()
+//     ]))
+//     .pipe(gulp.dest("build/css"))
+//     .pipe(minify())
+//     .pipe(rename("style.min.css"))
+//     .pipe(gulp.dest("build/css"))
+//     .pipe(server.stream());
+// });
+
+// gulp.task("server", function () {
+//   server.init({
+//     server: "build/",
+//     notify: false,
+//     open: true,
+//     cors: true,
+//     ui: false
+//   });
+
+//   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+//   gulp.watch("source/img/icons/*.svg", gulp.series("sprite", "html"));
+//   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+// });
+
+// gulp.task("refresh", function(done) {
+//   server.reload();
+//   done();
+// });
+
+// gulp.task("webp", function () {
+//   return gulp.src("source/img/**/*.{png,jpg}")
+//     .pipe(webp({ quality: 90 }))
+//     .pipe(gulp.dest("source/img"));
+// });
+
+// gulp.task("images", function () {
+//   return gulp.src("source/img/**/*.{png,jpg,svg}")
+//     .pipe(imagemin([
+//       imagemin.optipng({ optimizationLevel: 3 }),
+//       imagemin.jpegtran({ progressive: true }),
+//       imagemin.svgo()
+//     ]))
+//     .pipe(gulp.dest("build/img"));
+// });
+
+// gulp.task("html", function () {
+//   return gulp.src("source/*.html")
+//     .pipe(posthtml([
+//       include()
+//     ]))
+//     .pipe(gulp.dest("build"));
+// });
+
+// gulp.task("copy", function () {
+//   return gulp.src([
+//     "source/fonts/**/*.{woff,woff2}",
+//     "source/img/**/*.{png,jpg,svg,webp}",
+//     "source/js/**/*.js"
+//   ], {
+//       base: "source"
+//     })
+//     .pipe(gulp.dest("build"));
+// });
+
+// gulp.task("clean", function () {
+//   return del("build");
+// });
+
+// gulp.task("sprite", function () {
+//   return gulp.src([
+//     "source/img/icons/*.svg"
+//   ])
+//     .pipe(svgstore({
+//       inlineSvg: true
+//     }))
+//     .pipe(rename("sprite.svg"))
+//     .pipe(gulp.dest("build/img"));
+// });
+
+// gulp.task("build", function (done) {
+//   run(
+//     "clean",
+//     "copy",
+//     "css",
+//     "sprite",
+//     "html",
+//     done
+//   );
+// });
+
+// gulp.task("build", gulp.series("css", "sprite", "html"));
+// gulp.task("start", gulp.series("build", "server"));
+
 "use strict";
 
 var gulp = require("gulp");
@@ -16,10 +131,12 @@ var run = require("run-sequence");
 var svgstore = require("gulp-svgstore");
 var webp = require("gulp-webp");
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+gulp.task("style", function () {
+  gulp.src("source/sass/style.scss")
     .pipe(plumber())
-    .pipe(sass())
+    .pipe(sass({
+      includePaths: require('node-normalize-scss').includePaths
+    }))
     .pipe(postcss([
       autoprefixer()
     ]))
@@ -30,17 +147,17 @@ gulp.task("css", function () {
     .pipe(server.stream());
 });
 
-gulp.task("server", function () {
+gulp.task("serve", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("source/*.html", ["html"]).on("change", server.reload);
 });
 
 gulp.task("webp", function () {
@@ -97,7 +214,7 @@ gulp.task("build", function (done) {
   run(
     "clean",
     "copy",
-    "css",
+    "style",
     "sprite",
     "html",
     done
